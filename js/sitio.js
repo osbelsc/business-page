a2a96f873f74ae03bb8c0f8e45c10fb40d993365
osbelsc <osbelsc@estudiantes.uci.cu>
2021-12-08 23:01:46 -0600

    refinamiento de la pagina

--- js/sitio.js
+++ js/sitio.js
@@ -6,38 +6,38 @@ new WOW().init();
 /*----------------------------------
 Iniciamos smoothScroll (Scroll Suave)
 --------------------------------*/
-smoothScroll.init({
-    speed: 1000, // Integer. How fast to complete the scroll in milliseconds
-    offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
-
-});
+// smoothScroll.init({
+//     speed: 1000, // Integer. How fast to complete the scroll in milliseconds
+//     offset: 100, // Integer. How far to offset the scrolling anchor location in pixels
+//
+// });
 
 /*---------------------------------
     OCULTAR Y MOSTRAR BOTON IR ARRIBA
  ----------------------------------*/
-$(function () {
-    $(window).scroll(function () {
-        var scrolltop = $(this).scrollTop();
-        if (scrolltop >= 50) {
-            $(".ir-arriba").fadeIn();
-        } else {
-            $(".ir-arriba").fadeOut();
-        }
-    });
-
-});
+// $(function () {
+//     $(window).scroll(function () {
+//         var scrolltop = $(this).scrollTop();
+//         if (scrolltop >= 50) {
+//             $(".ir-arriba").fadeIn();
+//         } else {
+//             $(".ir-arriba").fadeOut();
+//         }
+//     });
+//
+// });
 
 /*---------------------------------
    CABECERA ANIMADA
  ----------------------------------*/
-$(window).scroll(function () {
-
-    var nav = $('.encabezado');
-    var scroll = $(window).scrollTop();
-
-    if (scroll >= 80) {
-        nav.addClass("fondo-menu");
-    } else {
-        nav.removeClass("fondo-menu");
-    }
-});
+// $(window).scroll(function () {
+//
+//     var nav = $('.encabezado');
+//     var scroll = $(window).scrollTop();
+//
+//     if (scroll >= 80) {
+//         nav.addClass("fondo-menu");
+//     } else {
+//         nav.removeClass("fondo-menu");
+//     }
+// });
